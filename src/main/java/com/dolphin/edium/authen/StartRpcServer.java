0a6ab538fe54d9107639c33e33d2ca9bc84cd13a
Hồ Văn Hiền <hohien@Hos-MacBook-Pro.lan>
2018-08-08 11:53:46 +0700

    edit heroku config

--- src/main/java/com/dolphin/edium/authen/StartRpcServer.java
+++ src/main/java/com/dolphin/edium/authen/StartRpcServer.java
@@ -3,7 +3,11 @@ package com.dolphin.edium.authen;
 import com.dolphin.edium.authen.service.GreeterService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.logging.Logger;
 
 public class StartRpcServer {
@@ -13,13 +17,27 @@ public class StartRpcServer {
 
     private void start() throws IOException {
         /* The port on which the server should run */
-        int port = 50051;
+        int port = 8088;
         server = ServerBuilder.forPort(port)
                 .addService(new GreeterService())
                 .build()
                 .start();
 
         logger.info("Server started, listening on " + port);
+
+        try {
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            String ip;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()))) {
+
+                //you get the IP as a String
+                ip = in.readLine();
+            }
+            logger.info("Server started, listening on ip: " + ip);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             // Use stderr here since the logger may have been reset by its JVM shutdown hook.
